2bfa807f41a56c98f0f767e87618f88a9be0641d
abrilbril <abrilbrilnu@gmail.com>
2022-07-27 12:55:32 -0300

    add form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3030;
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
 
 app.use(express.static("public"));
 
